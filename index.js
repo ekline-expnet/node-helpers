f2136e2bb69447a9a5db622573635738304e33a6
ekline-expnet <ekline@expression.net>
2016-10-21 09:36:43 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+  Stream: require("./isstream")
+}
---
+++ isstream.js
@@ -0,0 +1,29 @@
+var stream = require('stream')
+
+
+function isStream (obj) {
+  return (obj &&
+    (obj instanceof stream.Stream ||
+      typeof obj === 'object' && typeof obj.pipe === 'function')
+}
+
+
+function isReadable (obj) {
+  return isStream(obj) && typeof obj._read == 'function' && typeof obj._readableState == 'object'
+}
+
+
+function isWritable (obj) {
+  return isStream(obj) && typeof obj._write == 'function' && typeof obj._writableState == 'object'
+}
+
+
+function isDuplex (obj) {
+  return isReadable(obj) && isWritable(obj)
+}
+
+
+module.exports            = isStream
+module.exports.isReadable = isReadable
+module.exports.isWritable = isWritable
+module.exports.isDuplex   = isDuplex
